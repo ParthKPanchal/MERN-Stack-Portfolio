b6d31721b21f8038d64cbf90cd9d833d5275c6e6
Parth Panchal <panchalparth93@yahoo.in>
2026-05-18 16:27:33 +0530

    Fixed admin message API

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -21,7 +21,7 @@ const Admin = () => {
   const fetchMessages = async () => {
     try {
       const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/api/visitors`
+        `${import.meta.env.VITE_API_URL}/api/messages`
       );
 
       setMessages(response.data);
